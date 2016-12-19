85fe052a5b2f1add95c0e6121a542864be6db8ed
lvt <zero.lethanh@gmail.com>
2016-12-19 21:28:00 +0900

    画像キャッシュ化

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -14,9 +14,10 @@ class QRController extends Controller
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
             header('Content-Disposition: attachment; filename="' . basename($file) . '"');
-            header('Expires: 0');
-            header('Cache-Control: must-revalidate');
-            header('Pragma: public');
+//            header('Expires: 0');
+//            header('Cache-Control: must-revalidate');
+            header( 'Cache-Control: max-age=37739520, public' );
+//            header('Pragma: public');
             header('Content-Length: ' . filesize($file));
             readfile($file);
             exit;
