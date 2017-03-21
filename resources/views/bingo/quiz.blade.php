a5788d5610af7673ee880809942d8628de6768a1
lvt <zero.lethanh@gmail.com>
2017-03-21 18:19:38 +0900

    クイズ登録画面（http://160.16.52.175/bingo/quizzes）にも、１説明書２ゲスト登録３クイズ作成４ビンゴスタートのリンクタグ入れてほしいです。

--- resources/views/bingo/quiz.blade.php
+++ resources/views/bingo/quiz.blade.php
@@ -22,14 +22,38 @@ EOD;
     @include('bootstrap.sources')
     {{--<script src="https://unpkg.com/vue/dist/vue.js"></script>--}}
 
+    <style>
+        .control-buttons {
+            background-size: 100%;
+            width: 105px;
+            height: 52px;
+            border: none;
+        }
+    </style>
 </head>
 <body>
 
 <div class="container">
+    <div class="row" style="display: inline;">
+        <div style="float: left;"><h1>クイズ作成</h1></div>
+        <div style="float: right;">
 
-    <div class="text-center">
-        クイズ作成
+            <button style="background-image: url('/botann/newボタン1.png'); " onclick="window.open().location.href = '/bingo/description'" class="control-buttons">
+            </button>
+            <button onclick="location.href = '/bingo/upload-list'" style="background-image: url('/botann/newボタン2.png')" class="control-buttons">
+
+            </button>
+
+            <button onclick=" location.href = '/bingo/quizzes'" style="background-image: url('/botann/newボタン3.png');" class="control-buttons">
+
+            </button>
+            <button onclick=" location.href = '/bingo/start'" class="control-buttons" style="background-image: url('/botann/newボタン41.png')">
+
+            </button>
+
+        </div>
     </div>
+    
 
     <table class="table table-bordered">
         {{-- quiz head --}}
--- resources/views/bingo/uploadList.blade.php
+++ resources/views/bingo/uploadList.blade.php
@@ -38,7 +38,7 @@
         <img src="/img/rogo975-575.png" class="logo">
         <div style="float: right;">
 
-            <button style="background-image: url('/botann/newボタン1.png'); " class="control-buttons">
+            <button style="background-image: url('/botann/newボタン1.png'); " onclick="window.open().location.href = '/bingo/description'" class="control-buttons">
             </button>
             <button onclick="location.href = '/bingo/upload-list'"
                     style="background-image: url('/botann/newボタン2.png')"
