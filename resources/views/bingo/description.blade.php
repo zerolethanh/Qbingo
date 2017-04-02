f3ad5a0794e52439b11883bce5122d4a0b3dd9eb
lvt <zero.lethanh@gmail.com>
2017-04-03 02:44:27 +0900

    add:説明書ページにゲスト登録、クイズ作成、スタートとかのボタンが他のページ同様必要

--- resources/views/bingo/description.blade.php
+++ resources/views/bingo/description.blade.php
@@ -7,9 +7,44 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>説明書</title>
     @include('bootstrap.sources')
+    <style>
+        .control-buttons {
+            background-size: 100%;
+            width: 105px;
+            height: 52px;
+            border: none;
+        }
+        .logo {
+            width: 195px;
+            height: 115px;
+            position: absolute;
+            top: 0;
+            float: left;
+
+        }
+    </style>
 </head>
 <body>
 <div class="container">
+    <div class="row" style="display: inline;">
+        <img src="/img/rogo975-575.png" class="logo">
+        <div style="float: right;">
+
+            <button style="background-image: url('/botann/newボタン1.png'); " onclick="window.open().location.href = '/bingo/description'" class="control-buttons">
+            </button>
+            <button onclick="location.href = '/bingo/upload-list'" style="background-image: url('/botann/newボタン2.png')" class="control-buttons">
+
+            </button>
+
+            <button onclick=" location.href = '/bingo/quizzes'" style="background-image: url('/botann/newボタン3.png');" class="control-buttons">
+
+            </button>
+            <button onclick=" location.href = '/bingo/start'" class="control-buttons" style="background-image: url('/botann/newボタン41.png')">
+
+            </button>
+
+        </div>
+    </div>
     <div class="text-center">
         <img src="/img/des1.jpg" alt="" class="img-responsive center-block">
         <img src="/img/des2.jpg" alt="" class="img-responsive center-block">
